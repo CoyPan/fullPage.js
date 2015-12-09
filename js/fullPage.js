1d948a3394c9742f9b201ba1bf1d381a3dd09e91
coypan <pky1209@126.com>
2015-12-09 11:20:04 +0800

    update

--- js/fullPage.js
+++ js/fullPage.js
@@ -1,3 +1,44 @@
 (function(window,document){
 
+    /* set the style for the body and div */
+    function init(){
+      $("html").css({
+        "width":"100%",
+        "height":"100%",
+        "margin":"0px",
+        "padding":"0px"
+      });
+      $("body").css({
+        "width":"100%",
+        "height":"100%",
+        "overflow":"hidden",
+        "margin":"0px",
+        "padding":"0px"
+      });
+      $(".fullPage").css({
+        "position":"relative",
+        "height":"100%",
+        "width":"100%"
+      });
+    }
+
+    /* listen the scroll */
+    function listenScroll(){
+
+    }
+
+    /* function to control the page to slide */
+    function pageSlide(){
+
+    }
+
+    /* exec function */
+    function go(){
+      if($(".fullPage")){
+        init();
+      }
+
+    }
+    go();
+
 })(window,document);
